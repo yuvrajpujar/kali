04228ebe8ae0ac32c807b866ebb6cd5326af9440
yuvrajpujar <55084788+yuvrajpujar@users.noreply.github.com>
2025-12-23 05:17:38 +0000

    feat: add HelloController and web starter

---
+++ src/main/java/com/kali/HelloController.java
@@ -0,0 +1,16 @@
+package com.kali;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello, Kali!";
+    }
+
+}
